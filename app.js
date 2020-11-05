ab5ed5c689cb7e73a7121c36b6b0badd1b3477d9
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2020-11-06 00:34:18 +0100

    Started building mobile view

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger")
+  const navMobile = document.getElementById("navMobile")
+  //const navMobile = document.querySelector(".navMobile")
+
+  hamburger.addEventListener("click", () => {
+    document.getElementById("navMobile").classList.toggle("active")
+  })
+
+  navMobile.addEventListener("click", () => {
+    document.getElementById("navMobile").classList.toggle("active")
+    console.log("hej")
+  })
+})
