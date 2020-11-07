dfcf5ed4299bc44a3ed9e0dda6fb7de0901ea3cc
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2020-11-07 22:07:35 +0100

    added portfolio,contact and education to mobile view

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger")
-  const navMobile = document.getElementById("navMobile")
-  //const navMobile = document.querySelector(".navMobile")
+  //const navMobile = document.getElementById("navMobile")
+  const navMobile = document.querySelector(".navMobile")
 
   hamburger.addEventListener("click", () => {
-    document.getElementById("navMobile").classList.toggle("active")
+    //document.getElementById("navMobile").classList.toggle("active")
+    document.querySelector(".navMobile").classList.toggle("active")
   })
 
   navMobile.addEventListener("click", () => {
-    document.getElementById("navMobile").classList.toggle("active")
-    console.log("hej")
+    // document.getElementById("navMobile").classList.toggle("active")
+    document.querySelector(".navMobile").classList.toggle("active")
   })
 })
